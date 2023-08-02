891976cfa460e87c2530c5eff229ef26a9d9be8e
luca1777 <luca.alexandru17@yahoo.com>
2023-08-02 18:50:32 +0300

    added search bar and responsivness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ShoppingCart } from 'phosphor-react';
 import "./navbar.css";
+import logo from "./logo/logo.jpeg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <Link to="/">Luca-Shop</Link>
+      <Link to="/">
+        <img src={logo} alt="logo" />{" "}
+      </Link>
+      <form className="search-bar">
+        <input type="text" placeholder="search..."/>
+        <button type="submit">
+          <FontAwesomeIcon className="icon" icon={faMagnifyingGlass} />
+        </button>
+      </form>
       <div className="links">
-        <Link to="/cart">
-          <ShoppingCart size={32} />
-        </Link>
+        <div className="user">
+          <FontAwesomeIcon icon={faUser} />
+          <p>Contact</p>
+        </div>
+        <div className="cart">
+          <Link to="/cart">
+            <FontAwesomeIcon icon={faCartShopping} />
+          </Link>
+        </div>
       </div>
     </div>
   );
